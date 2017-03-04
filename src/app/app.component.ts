44c6d393abb5301d3f054823dc4bb0625b39b54b
dakaii <daiki815@gmail.com>
2017-03-04 10:57:38 +0900

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <user></user>
+    <ul>
+      <li><a routerLink='/'>Home</a></li>
+      <li><a routerLink='/about'>About</a></li>
+    </ul>
+    <hr />
+    <router-outlet></router-outlet>
     `,
 })
 export class AppComponent  {
-  name = 'Angular';
-  email = 'Angular@gmail.com';
-  address = {
-    street: '12 Main st',
-    city : 'Boston',
-    state: 'MA'
-  }
+
 }
